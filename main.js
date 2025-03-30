9c50bc721807e8037a2cdc5250dd72203a32c82c
gabrielpeixinh016 <gabrielpeixinh016@gmail.com>
2025-03-30 20:14:10 +0000

    finalmente é possivel calcular tudo o que o projeto se propoem, contudo,ainda falta coisas

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let bill = 0
  let tipPercentage = 0 
  let numberOfPeople = 0
- 
+ let buttonSelected = null
 
  let billInput = document.querySelector("#bill")
  billInput.addEventListener("input",receiveBillValue)
@@ -18,16 +18,46 @@ let bill = 0
      numberOfPeople = numberOfPeopleInput.valueAsNumber
      calculate()
  }
+
+ function receiveTipPercentage(value) {
+     if (buttonSelected !== null) {
+         buttonSelected.classList.remove("button-selected")
+     }
+ 
+     buttonSelected = document.querySelector(`#button-${value}`)
+     buttonSelected.classList.add("button-selected")
+     tipPercentage = value / 100
+ 
+     let customTipInput = document.querySelector("#custom-tip")
+     customTipInput.value = ""
+ 
+ }
  
  function receiveTipPercentage(value){
      let buttonSelected = document.querySelector
  
  }
- 
+ function receiveCustomTipPercentage() {
+    let customTipInput = document.querySelector("#custom-tip")
+    tipPercentage = customTipInput.valueAsNumber / 100
+
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected");
+        buttonSelected = null;
+    }
+    calculate();
+}
+
  function calculate(){
      if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
-         console.log('calcular')
-     }else{
-     console.log("ainda não é possivel calcular")
-     }
- }
+        let tipAmountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage ) / numberOfPeople
+        tipAmountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong")
+        let total = (bill / numberOfPeople) + tipAmountPerson
+        totalStrong.innerText = `$${total.toFixed(2)}`
+    }else{
+        console.log("ainda não é possivel calcular")
+    }
+}
